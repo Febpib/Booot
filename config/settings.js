fde339038f62844991f8564b74a347ab0a747308
Febpib <128316818+Febpib@users.noreply.github.com>
2023-05-27 16:43:50 +0700

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const chalk = require("chalk");
 // EDIT DISINI
-global.owner = ["6283820352529" "6283838887677]; // no own
+global.owner = ["6283820352529""6283838887677"]; // no own
 global.packname = "© sticker by FIKO'XD"; // nama pack sticker
 global.author = "FikoXdStore"; // nama author
 global.pic = "https://i.postimg.cc/bYsWpp4D/20230517-115351.jpg"; // url foto
